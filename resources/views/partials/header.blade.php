8ed62ca80889f53b87787946e0012b082457c471
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-08 16:20:50 +0100

    Import partials and yield parts

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="d-flex justify-content-between align-items-center py-5 bg-dark text-white">
+        <div class="logo h-100">
+            <img src="" alt="">
+        </div>
+        <ul class="h-100 d-flex list-unstyled">
+            <li class="d-inline-block">Ciao</li>
+        </ul>
+    </nav>
+</header>
