d23ceede46ec90ea09ea52210b353c450f4f06a7
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-08 16:39:26 +0100

    Create header nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,12 @@
 <header>
-    <nav class="d-flex justify-content-between align-items-center py-5 bg-dark text-white">
-        <div class="logo h-100">
-            <img src="" alt="">
-        </div>
-        <ul class="h-100 d-flex list-unstyled">
-            <li class="d-inline-block">Ciao</li>
-        </ul>
-    </nav>
+    <div class="header-container">        
+        <nav>
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/assets/img/dc-logo.png')}}" alt="logo">
+            </div>
+            <ul>
+                <li>Ciao</li>
+            </ul>
+        </nav>
+    </div>
 </header>
