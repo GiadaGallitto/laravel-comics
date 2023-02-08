9bb4e720aef2a6cbd653b1783be662034b6b30c7
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-08 16:54:00 +0100

    Implement header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,16 @@
                 <img src="{{ Vite::asset('resources/assets/img/dc-logo.png')}}" alt="logo">
             </div>
             <ul>
-                <li>Ciao</li>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
             </ul>
         </nav>
     </div>
