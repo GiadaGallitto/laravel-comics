39b39b9eb1c001a5a080c330cb0bf931e6662fee
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-08 17:29:37 +0100

    Add array to db.php

--- config/db.php
+++ config/db.php
@@ -73,6 +73,28 @@
                 "series" => "Catwoman",
                 "type" => "graphic novel"
             ]
+        ],
+        "iconList" => [
+            [
+                "icon" => "buy-comics-digital-comics.png",
+                "text" => "Digital Comics",
+            ],
+            [
+                "icon" => "buy-comics-merchandise.png",
+                "text" => "Merchandise",
+            ],
+            [
+                "icon" => "buy-comics-subscriptions.png",
+                "text" => "Subscription",
+            ],
+            [
+                "icon" => "buy-comics-shop-locator.png",
+                "text" => "Comic Shop Locator",
+            ],
+            [
+                "icon" => "buy-dc-power-visa.svg",
+                "text" => "Dc Power Visa",
+            ]
         ]
-    ]
+            ];
 ?>
