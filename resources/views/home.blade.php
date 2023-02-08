347342cc365d8aa9348f9f577a1e3051864ef9f8
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-08 17:19:43 +0100

    Create comics section of main and jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.app')
 
 @section('main-content')
-    
+<section class="content-part">
+    <div class="jumbotron">
+
+    </div>
+
+    <div class="comics-grid">
+        <div class="current blue-btn"><h2>Current Series</h2></div>
+        @foreach ($comicsList as $comicEl)            
+        <div class="comics-card">
+            <div class="image-box">
+                <img src="{{ $comicEl['thumb'] }}" alt="">
+            </div>
+            <div class="text-box">
+                <p>{{$comicEl['series']}}</p>
+            </div>
+        </div>
+        @endforeach
+    </div>
+
+    <div class="more-btn blue-btn">Load More</div>
+</section>
+
+<section class="icons-nav">
+    <div class="icons-container">
+        <div class="banner-element">
+            
+        </div>
+    </div>
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
+    $comicsList = config('db.comicsList');
+    return view('home', compact("comicsList"));
+}) ->name('home');
